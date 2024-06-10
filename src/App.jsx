d7c284d9d7560ef4ce5d70dc8470352a8308ca6a
Layton5 <kayodematthieu@gmail.com>
2024-06-10 13:18:23 +0100

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,43 @@
-// import themeConfig from "./theme.cofig";
-
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Dashboard from "./views/Dashboard/Dashboard";
-import Product from "./views/Product/Product";
-import Orders from "./views/Orders/Orders";
-import Customers from "./views/Customers/Customers";
-import CurrencyExchange from "./views/CurrencyExchange/CurrencyExchange";
-import Courier from "./views/Courier/Courier";
-import Wallet from "./views/Wallet/Wallet";
-import Support from "./views/Support/Support";
-import Settings from "./views/Settings/Settings";
-import Login from "./Login/Login";
-
-function App() {
-  // const theme = themeConfig.token;
-  // const router = createBrowserRouter([
-  //   {
-  //     path: "/",
-  //     element: <Layout />,
-  //     children: [
-  //       {
-  //         path: "Dashboard",
-  //         element: <Dashboard />,
-  //       },
-  //       {
-  //         path: "Products",
-  //         element: <Product />,
-  //       },
-  //       {
-  //         path: "Orders",
-  //         element: <Orders />,
-  //       },
-  //       {
-  //         path: "Customers",
-  //         element: <Customers />,
-  //       },
-  //       {
-  //         path: "Currency Exchange",
-  //         element: <CurrencyExchange />,
-  //       },
-  //       {
-  //         path: "Courier",
-  //         element: <Courier />,
-  //       },
-  //       {
-  //         path: "Wallet",
-  //         element: <Wallet />,
-  //       },
-  //       {
-  //         path: "Support",
-  //         element: <Support />,
-  //       },
-  //       {
-  //         path: "Settings",
-  //         element: <Settings />,
-  //       },
-  //     ],
-  //   },
-  // ]);
-
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Dashboard } from "./views/Dashboard/Dashboard";
+const App = () => {
   return (
-    <>
-      <Dashboard />
-      {/* <Login /> */}
-    </>
+    <Router>
+      {/* <Dashboard /> */}
+      <Switch>
+        <Route exact path="/Dashboard" component={Dashboard} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
+
+// // import themeConfig from "./theme.cofig";
+
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import Dashboard from "./views/Dashboard/Dashboard";
+// import Product from "./views/Product/Product";
+// import Orders from "./views/Orders/Orders";
+// import Customers from "./views/Customers/Customers";
+// import CurrencyExchange from "./views/CurrencyExchange/CurrencyExchange";
+// import Courier from "./views/Courier/Courier";
+// import Wallet from "./views/Wallet/Wallet";
+// import Support from "./views/Support/Support";
+// import Settings from "./views/Settings/Settings";
+// import Login from "./Login/Login";
+
+// f
+
+//   return (
+//     <>
+//       <Dashboard />
+//       {/* <Login /> */}
+//     </>
+//   );
+// }
+
+// export default App;
--- src/Layout/Sidebar.jsx
+++ src/Layout/Sidebar.jsx
@@ -9,48 +9,74 @@ import walletIcon from "../utils/icons/wallet.svg";
 import supportIcon from "../utils/icons/support.svg";
 import settingIcon from "../utils/icons/settings.svg";
 const { Title } = Typography;
+// const active=
 
 const Sidebar = () => {
   const side = [
     {
       name: "Dashboard",
       icon: homeIcon,
+      link: "/Board",
     },
     {
       name: "Products",
       icon: productIcon,
+      link: "/Products",
     },
     {
       name: "Orders",
       icon: orderIcon,
+      link: "/Orders",
     },
     {
       name: "Customers",
       icon: customerIcon,
+      link: "/Customers",
     },
     {
       name: "Currency Exchange",
       icon: currencyIcon,
+      link: "/CurrencyExchange",
     },
     {
       name: "Couriers",
       icon: courierIcon,
+      link: "/Courier",
     },
     {
       name: "Wallet",
       icon: walletIcon,
+      link: "/Wallet",
     },
     {
       name: "Support",
       icon: supportIcon,
+      link: "/Support",
     },
     {
       name: "Settings",
       icon: settingIcon,
+      link: "/Settings",
     },
   ];
   return (
     <>
+      {/* <ul>
+        {side.map((item, index) => {
+          return (
+            <li
+              key={index}
+              onClick={() => {
+                window.location.pathname = item.link;
+              }}
+            >
+              <img src={item.icon} alt="" />
+              <div>{item.name}</div>
+            </li>
+          );
+        })}
+      </ul> */}
+
       <div
         style={{
           width: "39vh",
@@ -64,7 +90,14 @@ const Sidebar = () => {
           style={{ width: "100%", padding: 12 }}
         >
           {side.map((item, index) => (
-            <div key={index} style={{ display: "flex", marginBottom: 0 }}>
+            <div
+              key={index}
+              onClick={() => {
+                window.location.pathname = item.link;
+              }}
+              style={{ display: "flex", marginBottom: 0, cursor: "pointer" }}
+              id={window.location.pathname == item.link ? "active" : ""}
+            >
               <img src={item.icon} alt="" style={{ marginTop: "12px" }} />
 
               <Title
