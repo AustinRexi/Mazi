5e55c80c8beca034e5a0ff35e3b5c829eebae77d
AustinRexi <Austinaminu@gmail.com>
2024-08-22 22:48:11 +0100

    courier

--- src/Layout/Sidebar.jsx
+++ src/Layout/Sidebar.jsx
@@ -8,91 +8,107 @@ import courierIcon from "../utils/icons/courier.svg";
 import walletIcon from "../utils/icons/wallet.svg";
 import supportIcon from "../utils/icons/support.svg";
 import settingIcon from "../utils/icons/settings.svg";
+import homefilled from "../utils/icons/homefilled.svg";
+import productfilled from "../utils/icons/productfilled.svg";
+import orderfilled from "../utils/icons/ordersfilled.svg";
+import customersfilled from "../utils/icons/customersfilled.svg";
+import currencyfilled from "../utils/icons/currencyfilled.svg";
+import courierfilled from "../utils/icons/courierfilled.svg";
+import walletfilled from "../utils/icons/walletfilled.svg";
+import supportfilled from "../utils/icons/supportfilled.svg";
+import settingsfilled from "../utils/icons/settingsfilled.svg";
+
 import { NavLink } from "react-router-dom";
 const { Title } = Typography;
 
 const Sidebar = () => {
-  const navLinkStyles = ({ isActive }) => {
-    return {
-      top: "12px",
-      marginTop: isActive ? "6px" : "",
-      display: "flex",
-      paddingLeft: "40px",
-
-      alignItems: "start",
-
-      color: isActive ? "cyan" : "orange",
-      borderLeft: isActive ? "5px solid #034147" : "none",
-      height: isActive ? "56px" : "auto", // Set height to "24px" when active, otherwise "auto"
-      borderRadius: isActive ? " 6px " : "0", // Adjust border-radius if necessary
-      backgroundColor: isActive ? "#F2FBFB" : "",
-      paddingBotom: "10px",
-    };
-  };
+  const navLinkStyles = ({ isActive }) => ({
+    top: "12px",
+    marginTop: isActive ? "6px" : "",
+    display: "flex",
+    paddingLeft: "40px",
+    alignItems: "start",
+    color: isActive ? "cyan" : "orange",
+    borderLeft: isActive ? "5px solid #034147" : "none",
+    height: isActive ? "56px" : "auto",
+    borderRadius: isActive ? "6px" : "0",
+    backgroundColor: isActive ? "#F2FBFB" : "",
+    paddingBottom: "10px",
+  });
 
   const side = [
     {
       name: "Dashboard",
       icon: homeIcon,
+      activeIcon: homefilled,
       link: "/Board",
     },
     {
       name: "Products",
       icon: productIcon,
+      activeIcon: productfilled,
       link: "/Products",
     },
     {
       name: "Orders",
       icon: orderIcon,
+      activeIcon: orderfilled,
       link: "/Orders",
     },
     {
       name: "Customers",
       icon: customerIcon,
+      activeIcon: customersfilled,
       link: "/Customers",
     },
     {
       name: "Currency Exchange",
       icon: currencyIcon,
+      activeIcon: currencyfilled,
       link: "/CurrencyExchange",
     },
     {
       name: "Couriers",
       icon: courierIcon,
+      activeIcon: courierfilled,
       link: "/Courier",
     },
     {
       name: "Wallet",
       icon: walletIcon,
+      activeIcon: walletfilled,
       link: "/Wallet",
     },
     {
       name: "Support",
       icon: supportIcon,
+      activeIcon: supportfilled,
       link: "/Support",
     },
     {
       name: "Settings",
       icon: settingIcon,
+      activeIcon: settingsfilled,
       link: "/Settings",
     },
   ];
+
   return (
-    <>
-      <div
-        style={{
-          width: "272px",
-          minHeight: "100vh",
-          boxShadow: "0 0 10px rgba(0, 0, 0, 0.2)",
-        }}
+    <div
+      style={{
+        width: "272px",
+        minHeight: "100vh",
+        boxShadow: "0 0 10px rgba(0, 0, 0, 0.2)",
+      }}
+    >
+      <Space
+        direction="vertical"
+        size="8"
+        style={{ width: "max-content", padding: 7, height: "160vh" }}
       >
-        <Space
-          direction="vertical"
-          size="8"
-          style={{ width: "max-content", padding: 7, height: "160vh" }}
-        >
-          {side.map((item) => (
-            <NavLink key={item.name} to={item.link} style={navLinkStyles}>
+        {side.map((item) => (
+          <NavLink key={item.name} to={item.link} style={navLinkStyles}>
+            {({ isActive }) => (
               <nav
                 style={{
                   display: "flex",
@@ -102,9 +118,7 @@ const Sidebar = () => {
                   justifyItems: "flex-start",
                 }}
               >
-                {navLinkStyles}
-                <img src={item.icon} alt="" />
-
+                <img src={isActive ? item.activeIcon : item.icon} alt="" />
                 <Title
                   level={4}
                   style={{
@@ -118,11 +132,11 @@ const Sidebar = () => {
                   {item.name}
                 </Title>
               </nav>
-            </NavLink>
-          ))}
-        </Space>
-      </div>
-    </>
+            )}
+          </NavLink>
+        ))}
+      </Space>
+    </div>
   );
 };
 
